618d8f7c6880c9bebefdb560312bbacdb71bd782
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-09-06 21:30:11 +0530

    Partition

---
+++ TelCo.ColorCoder/DataDeclaration.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    partial class Program
+    {
+        /// <summary>
+        /// Array of Major colors
+        /// </summary>
+        private static Color[] colorMapMajor;
+        /// <summary>
+        /// Array of minor colors
+        /// </summary>
+        private static Color[] colorMapMinor;
+        /// <summary>
+        /// data type defined to hold the two colors of color pair
+        /// </summary>
+        internal class ColorPair
+        {
+            internal Color majorColor;
+            internal Color minorColor;
+            public override string ToString()
+            {
+                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+            }
+        }
+        /// <summary>
+        /// Static constructor required to initialize static variable
+        /// </summary>
+        static Program()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+
+        /// <summary>
+        /// Given a pair number function returns the major and minor colors in that order
+        /// </summary>
+        /// <param name="pairNumber">Pair number of the color to be fetched</param>
+        /// <returns></returns>
+    }
+}
